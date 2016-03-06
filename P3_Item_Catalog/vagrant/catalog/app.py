d01d929dd579209de51c76b0291ef1d54d184e84
arvin-dwarka <arvin@bench.co>
2016-03-05 22:18:43 -0800

    fix some typos in P3

--- P3_Item_Catalog/vagrant/catalog/app.py
+++ P3_Item_Catalog/vagrant/catalog/app.py
@@ -17,6 +17,12 @@ from oauth2client.client import FlowExchangeError
 
 app = Flask(__name__)
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Coktail App"
+
+
+# Connect to Database and create database session
 urlparse.uses_netloc.append("postgres")
 # Gets DB variable from Heroku. If no DBURL is present, uses local appreview.db
 dburl = urlparse.urlparse(os.getenv("DATABASE_URL", "/cocktailsdb"))
@@ -30,22 +36,29 @@ session = DBSession()
 #login
 @app.route('/login')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.
-        digits) for x in xrange(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
     login_session['state'] = state
-    return 'The current session state is %s' %login_session['state']
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    Connect to Google OAuth API and generate a user login session.
+    """
+
+    # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+    # Obtain authorization code
     code = request.data
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(r'/var/www/appreview/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
@@ -103,17 +116,96 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
 
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    # see if user exists, if it doesn't make a new one
+    user_id = getUserID(data["email"])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+    
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
-    return output    
+    output = 'Login Successful.'
+    return output
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    """
+    Connect to FB Graph API and generate a user login session.
+    """
+
+    # Check state parameter to ensure we're logging in from the current state.
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+
+    # Validate request with Facebook to generate token
+    app_id = json.loads(open(r'/var/www/appreview/fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open(r'fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # strip expire tag from access token
+    token = result.split("&")[0]
+    # Use token to get user info from API
+    url = 'https://graph.facebook.com/v2.4/me?fields=name,id,email&%s' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+    print url, "\n"
+    print data, "\n"
+    
+    # Populate user data into login_session object
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout,
+    # let's strip out the information before the equals sign in our token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists in our application
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    flash("Now logged in as %s" % login_session['username'])
+    output = 'Login Successful.'
+    return output
+
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    """
+    Execute request to remove access from app.
+    and to revoke access from token with Facebook
+    """
+    
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
 
 
 #app stuffs
@@ -160,5 +252,6 @@ def JSONSingleCocktail(category_id, cocktail_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
